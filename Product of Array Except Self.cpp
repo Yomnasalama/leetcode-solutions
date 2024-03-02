5d4143b92f79dbb8f85b250faf185ada875a424e
Yomnasalama <64813394+Yomnasalama@users.noreply.github.com>
2024-03-02 15:57:32 +0200

    Create Product of Array Except Self.cpp
    Product of Array Except Self problem solution

---
+++ Product of Array Except Self.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> prefix (nums.size(),1);
+        vector<int> suffix (nums.size(),1);
+        vector<int> res (nums.size(),1);
+        for(int i = 1; i < nums.size(); i++) {
+            prefix[i] = nums[i - 1] * prefix[i-1];
+        }
+        for(int i = nums.size() - 2; i >= 0; i--) {
+            suffix[i] = suffix[i+1] * nums[i+1];
+        }
+        for(int i = 0; i < nums.size(); i++) {
+            res[i] = prefix[i]*suffix[i];
+        }
+        return res;
+    }
+};
